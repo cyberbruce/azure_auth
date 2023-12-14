7b2746adda306419c440b134da23b1e17286f9ea
Bruce Martin <cyber.bruce@yahoo.com>
2023-12-14 15:59:35 -0800

    ui updates

--- app/controllers/callbacks_controller.rb
+++ app/controllers/callbacks_controller.rb
@@ -1,5 +1,5 @@
 class CallbacksController <  Devise::OmniauthCallbacksController
-	
+
   def azure_activedirectory_v2
 		@user = User.from_omniauth(request.env["omniauth.auth"])
     puts request.env["omniauth.auth"]	 
--- app/controllers/loads_controller.rb
+++ app/controllers/loads_controller.rb
@@ -1,5 +1,5 @@
 class LoadsController < ApplicationController
-  before_action :authenticate_user!
+  
   before_action :set_load, only: %i[ show edit update destroy ]
 
   # GET /loads or /loads.json
